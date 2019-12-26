6078ee7f9f1d7688761d89ab0a596abb70166117
Nicholas Marcopoli <nick@mmarcopoli01-mac.local>
2019-12-26 02:11:56 -0500

    starting out

--- 02-starting-out.hs
+++ 02-starting-out.hs
@@ -10,14 +10,17 @@
  -}
 
 -- Find the penultimate element in list l
+penultimate :: [a] -> a
 penultimate l = last (init l)
 
 -- Find the element at index k in list l
 -- For example: "findK 2 [0,0,1,0,0,0]" returns 1
-findK k l = undefined
+findK :: Int -> [a] -> a
+findK k l = last (take (k + 1) l)
 
 -- Determine if list l is a palindrome
-isPalindrome l = undefined
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome l = l == reverse l
 
 {-
  - Duplicate the elements in list xs, for example "duplicate [1,2,3]" would give the list [1,1,2,2,3,3]
@@ -26,31 +29,41 @@ isPalindrome l = undefined
  -
  - For example: concat [[1,2,3],[3,4,5]] returns [1,2,3,3,4,5]
  -}
-duplicate xs = undefined
+duplicate :: [a] -> [a]
+duplicate xs = concat [[x, x] | x <- xs]
 
 {-
  - Imitate the functinality of zip
  - The function "min x y" returns the lower of values x and y
  - For example "ziplike [1,2,3] ['a', 'b', 'c', 'd']" returns [(1,'a'), (2, 'b'), (3, 'c')]
  -}
-ziplike xs ys = undefined
+ziplike :: [a] -> [b] -> [(a, b)]
+ziplike _ [] = [] -- base case, when b has no more elements
+ziplike [] _ = [] -- base case, when a has no more elements
+ziplike (x:xs) (y:ys) = (x,y):ziplike xs ys
 
 -- Split a list l at element k into a tuple: The first part up to and including k, the second part after k
 -- For example "splitAtIndex 3 [1,1,1,2,2,2]" returns ([1,1,1],[2,2,2])
-splitAtIndex k l = undefined
+splitAtIndex :: Int -> [b] -> ([b], [b])
+splitAtIndex k l = (take (k+1) l, reverse (take (length l - (k + 1)) (reverse l)))
+
 
 -- Drop the element at index k in list l
 -- For example "dropK 3 [0,0,0,1,0,0,0]" returns [0,0,0,0,0,0]
-dropK k l = undefined
+dropK :: Int -> [a] -> [a]
+dropK k l = concat [(take k l), (drop (k+1) l)]
 
 -- Extract elements between ith and kth element in list l. Including i, but not k
 -- For example, "slice 3 6 [0,0,0,1,2,3,0,0,0]" returns [1,2,3]
-slice i k l = undefined
+slice :: Int -> Int -> [a] -> [a]
+slice i k l = drop i (take k l)
 
 -- Insert element x in list l at index k
 -- For example, "insertElem 2 5 [0,0,0,0,0,0]" returns [0,0,0,0,0,2,0]
-insertElem x k l = undefined
+insertElem :: a -> Int -> [a] -> [a]
+insertElem x k l = concat [take k l, x : drop k l]
 
 -- Rotate list l n places left.
 -- For example, "rotate 2 [1,2,3,4,5]" gives [3,4,5,1,2]
-rotate n l = undefined
+rotate :: Int -> [a] -> [a]
+rotate n l = concat [drop n l, take n l]
